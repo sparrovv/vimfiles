84977484ada7d4174854c3e091a83b40022991f2
Michal Wrobel <sparrovv@gmail.com>
2012-11-08 22:11:27 +0100

    Improving install script by adding git clone vundle

--- install.sh
+++ install.sh
@@ -5,6 +5,11 @@
 echo 'installing ack'
 brew install ack
 
+if [ ! -d ~/.vim/bundle/vundle ]; then
+  echo 'adding vundle - vim plugins package manger'
+  git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+fi
+
 echo 'linking vimrc and ackrc'
 ln -sf ~/.vim/vimrc ~/.vimrc
 ln -sf ~/.vim/ackrc ~/.ackrc
